2dcb1f357e9c015c25d3410e12ae66bf9887ebd6
nblancoz <nblancozappala@gmail.com>
2024-01-13 21:08:31 +0100

    message shown in service

--- src/features/services/serviceService.js
+++ src/features/services/serviceService.js
@@ -4,7 +4,6 @@ const API_URL = "http://localHost:8080/services/";
 const token = JSON.parse(localStorage.getItem("token"));
 
 const create = async (data) => {
-  console.log(data)
   const res = await axios.post(API_URL + "create", data, {
     headers: {
       Authorization: token,
