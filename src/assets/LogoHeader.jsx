a60b223e266e7db56fa592c282beb080bc07c91b
nblancoz <nblancozappala@gmail.com>
2024-01-17 12:09:57 +0100

    stile buildingPage modified

---
+++ src/assets/LogoHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LogoHeader = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="28"
+      viewBox="0 0 16 28"
+      fill="none"
+    >
+      <path
+        d="M14 26L2 14.001L14 2"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="square"
+      />
+    </svg>
+  );
+};
+
+export default LogoHeader;
