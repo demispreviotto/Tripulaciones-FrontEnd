59d802b5e2386084c9d60676414f1342912d4255
nblancoz <nblancozappala@gmail.com>
2024-01-17 17:26:23 +0100

    handleGoBuildings added to home

---
+++ src/assets/Logo_VerFincas.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Logo_VerFincas = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="18"
+      viewBox="0 0 10 18"
+      fill="none"
+    >
+      <path
+        d="M1.75 2L8.75 9L1.75 16"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="square"
+      />
+    </svg>
+  );
+};
+
+export default Logo_VerFincas;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -5,59 +5,67 @@ import { getAllBuildings } from "../../../features/building/buildingSlice";
 import { fetchAndCreateIncidences } from "../../../features/incidence/incidenceSlice";
 import Preloader from "../Preloader/Preloader";
 import BuildingCheck from "../../Buildings/BuildingCheck";
-import "./Home.scss"
+import "./Home.scss";
+import Logo_VerFincas from "../../../assets/Logo_VerFincas";
 
 const tokenLocal = JSON.parse(localStorage.getItem("token"));
 
 const Home = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.auth.user);
-    const token = useSelector((state) => state.auth.token);
-    const buildings = useSelector((state) => state.building.buildings);
-    const [formattedDate, setFormattedDate] = useState("");
-    useEffect(() => {
-        if (!user) {
-            navigate("/inicio-sesion");
-        } else {
-            // dispatch(getAllBuildings());
-            // dispatch(fetchAndCreateIncidences());
-            const formattedDate = new Date().toLocaleDateString("es-ES", {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-            });
-            setFormattedDate(formattedDate);
-        }
-    }, []);
-    useEffect(() => {
-        if (token === tokenLocal) {
-            dispatch(getAllBuildings());
-        }
-    }, [])
-
-    if (!buildings || !user || !token) {
-        return <Preloader />;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const token = useSelector((state) => state.auth.token);
+  const buildings = useSelector((state) => state.building.buildings);
+  const [formattedDate, setFormattedDate] = useState("");
+  useEffect(() => {
+    if (!user) {
+      navigate("/inicio-sesion");
+    } else {
+      // dispatch(getAllBuildings());
+      // dispatch(fetchAndCreateIncidences());
+      const formattedDate = new Date().toLocaleDateString("es-ES", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      });
+      setFormattedDate(formattedDate);
     }
+  }, []);
+  useEffect(() => {
+    if (token === tokenLocal) {
+      dispatch(getAllBuildings());
+    }
+  }, []);
 
-    return (
-        <div className="home">
-            <div className="greetings">
-                <h2>
-                    Buenos dias,
-                    <br /> {user.firstName} {user.lastName}
-                </h2>
-                <p>{formattedDate}</p>
-            </div>
-            <div className="building-check-container">
-                <h3 className="building-check-header">Fincas por revisar hoy</h3>
-                <div className="building-check">
-                    <BuildingCheck buildings={buildings} />
-                </div>
-            </div>
+  if (!buildings || !user || !token) {
+    return <Preloader />;
+  }
+
+  const handleGoBuildings = () => {
+    navigate("/fincas");
+  };
+
+  return (
+    <div className="home">
+      <div className="greetings">
+        <h2>
+          Buenos dias,
+          <br /> {user.firstName} {user.lastName}
+        </h2>
+        <p>{formattedDate}</p>
+      </div>
+      <div className="building-check-container">
+        <h3 className="building-check-header">Fincas por revisar hoy</h3>
+        <div className="building-check">
+          <BuildingCheck buildings={buildings} />
         </div>
-    );
+        <div className="see-all-buildings" onClick={handleGoBuildings}>
+          Ver todas las fincas <Logo_VerFincas />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
